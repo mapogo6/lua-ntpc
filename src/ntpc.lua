d93f368975f19f6e6417512d64c63253ec03e13c
mapogo6 <10045819+mapogo6@users.noreply.github.com>
2026-01-12 08:40:35 -0800

    error handling

--- src/ntpc.lua
+++ src/ntpc.lua
@@ -1,19 +1,43 @@
 #! /usr/bin/env lua
 
+------------------------------------------------------------------------------------
+-- The MIT License (MIT)
+--
+-- Copyright (c) 2026 David Westen
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+------------------------------------------------------------------------------------
+
 local socket = require("socket")
 local struct = require("struct")
 local getopt = require("getopt")
 
-local ntp_packet_size <const> = 48
-local ntp_format <const> = ">BBBBIIIIIIIIIII"
+local ntp_packet_size <const>     = 48
+local ntp_format <const>          = ">BBBBIIIIIIIIIII"
 local ntp_timestamp_delta <const> = 2208988800
-local date_format <const> = "!%Y-%m-%dT%H:%M:%S" 
+local date_format <const>         = "!%Y-%m-%dT%H:%M:%S" 
 
 local ntp_server = "pool.ntp.org"
 local ntp_port = 123
 local timeout = 0
 
-help = function()
+local help = function(code)
     print("Usage: ntp [options] <ntp-server>")
     print("")
     print("Options:")
@@ -23,49 +47,49 @@ help = function()
     print("  -v        verbose output")
     print("Arguments:")
     print("  <ntp-server> the server to call, default " .. ntp_server)
-    os.exit(0)
+    os.exit(code or 0)
 end
 
 local opts = {}
-local opts_result = getopt.std("hp:t:v", opts)
+if not getopt.std("hp:t:v", opts) then
+    help(1)
+end
 
-if not opts_result or opts['h'] then
+if opts.h then
     help()
 end
 
 if opts.p then
-    ntp_port = tonumber(opts.p)
+    ntp_port = tonumber(opts.p) or help(1)
 end
 
 if opts.t then
-    timeout = tonumber(opts.t)
+    timeout = tonumber(opts.t) or help(1)
 end
 
-local verbose = opts.v or false
+-- function that will either call print or do nothing
+local log = opts.v and print or function(...) end
+
 ntp_server = arg[getopt.get_optind()] or ntp_server
 
 local dns, error = socket.dns.getaddrinfo(ntp_server)
-if dns == nil or #dns == 0 then
-    io.stderr:write(error .. "\n")
+if not dns then
+    io.stderr:write("getaddrinfo: " .. error .. "\n")
     os.exit(1)
 end
 
 local udp, error = socket.udp()
-if udp == nil then
-    io.stderr:write(error .. "\n")
+if not udp then 
+    io.stderr:write("socket:" .. error .. "\n")
     os.exit(1)
 end
 
-if timeout > 0 then
-udp:settimeout(timeout)
+if timeout > 0 and not socket.try(udp:settimeout(timeout)) then
+    io.stderr:write("settimeout: error\n")
+    os.exit(1)
 end
 
-local log = function(...)
-    if not verbose then return end
-    print(...)
-end
-
-local ntp_request = function(udp, addr, port)
+local ntp_request = socket.protect(function(udp, addr, port)
     local ntp_request = struct.pack(ntp_format,
     0x1b, 0, 0, 0,                      -- (li, vn, mode), stratum, poll, precision
     0, 0, 0,                            -- delay, dispersion, id
@@ -74,27 +98,20 @@ local ntp_request = function(udp, addr, port)
     0, 0,                               -- receive timestamp
     os.time() + ntp_timestamp_delta, 0) -- transmit timestamp
 
-    udp:setpeername(addr, port)
+    socket.try(udp:setpeername(addr, port))
 
-    local sent, error = udp:send(ntp_request)
-    if sent == nil or sent ~= ntp_packet_size then
-        io.stderr:write("send failed: " .. error .. "\n")
-        return nil
+    local sent = socket.try(udp:send(ntp_request))
+    if sent ~= ntp_packet_size then
+        error("sent " .. sent .. " bytes but expected to send " .. ntp_packet_size .. "bytes")
     end
 
-    local data, error = udp:receive()
-    if data == nil then
-      io.stderr:write("recv failed: " .. error .. "\n")
-      return nil
-    end
-
-    return data
-end
+    return socket.try(udp:receive())
+end)
 
 local key, value = next(dns)
 while key and value do
     log("connecting to " .. ntp_server .. ":" .. ntp_port .. " ('" .. value.addr .. "')")
-    local response, error = ntp_request(udp, value.addr, ntp_port)
+    local response, error  = ntp_request(udp, value.addr, ntp_port)
     if response then
         local t4_sec = os.time() + ntp_timestamp_delta
 
@@ -113,6 +130,11 @@ while key and value do
 
         print("time: " .. os.date(date_format, math.floor(ntp_time)))
         os.exit(0)
+    else
+        log(error)
     end
     key, value = next(dns, key)
 end
+
+io.stderr:write("could not reach " .. ntp_server .. ":" .. ntp_port .. "\n")
+os.exit(1)
